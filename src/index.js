0b5d7765b7238103af61bf8cb6053c7e41bacff4
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-10 21:20:54 +0100

    Added functional dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import "./styles.css";
+import './styles.css';
+
+const menu = document.getElementById('dropdown-header');
+const hidden = document.querySelector('.hidden');
+
+function toggleClass(parent, child, classList) {
+  parent.addEventListener('click', () => {
+    child.classList.toggle(classList);
+  })
+}
+
+function addClassOnHover(parent, child, classList) {
+  parent.addEventListener('mouseover', () => {
+    child.classList.remove(classList);
+  })
+  parent.addEventListener('mouseleave', () => {
+    child.classList.add(classList);
+  })
+}
+
+addClassOnHover(menu, hidden, 'hidden');
