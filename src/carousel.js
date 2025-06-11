accf2c9867a9d8b1425d78fc2ccac98c7cb3e79b
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-11 14:36:53 +0100

    added slide show functionality

--- src/carousel.js
+++ src/carousel.js
@@ -1,21 +1,43 @@
 let currentSlideIdx = 0;
 const slides = document.querySelectorAll('.content');
 
-// Moves one slide forward or backwards.
-export function nextSlide(step) {
-  currentSlideIdx = (currentSlideIdx + step + slides.length) % slides.length;
-
-  classListToggle(slides, 'invisible');
-}
-
-// Jumps between slides.
-export function jumpToSlide(n) {
-  currentSlideIdx = n;
-  classListToggle(slides, 'invisible');
-}
-
 // Helper function to add or remove classlist.
 function classListToggle(element, attribute) {
   element.forEach(s => s.classList.add(attribute));
   element[currentSlideIdx].classList.remove(attribute);
+}
+
+// Moves one slide forward or backwards.
+export function nextSlide(step) {
+  currentSlideIdx = (currentSlideIdx + step + slides.length) % slides.length;
+  classListToggle(slides, 'invisible');
+}
+
+// Jumps between slides.
+export function jumpToSlide(element) {
+  element.forEach((el) => {
+    el.addEventListener('click', () => {
+      currentSlideIdx = el.dataset.value;
+      classListToggle(slides, 'invisible');
+    })
+  })
+}
+
+// Adds border around current slide indicator.
+export function indicateActive(element, clList) {
+  element.forEach((el) => {
+    el.addEventListener('click', () => {
+      element.forEach((e) => e.classList.remove(clList));
+      el.classList.add(clList);
+    })
+  })
+}
+
+export function slideShow() {
+    currentSlideIdx++;
+
+    slides.forEach((slide) => slide.classList.add('invisible'));
+    if (currentSlideIdx > slides.length) currentSlideIdx = 1;
+    slides[currentSlideIdx-1].classList.remove('invisible');
+    setTimeout(slideShow, 4000);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
 import { toggleClass, addClassOnHover } from './dropdown.js';
-import { nextSlide, jumpToSlide } from './carousel.js' 
+import { nextSlide, jumpToSlide, indicateActive, slideShow } from './carousel.js' 
 
 const menu = document.getElementById('dropdown-header');
 const hidden = document.querySelector('.hidden');
@@ -9,17 +9,21 @@ const nextBtn = document.querySelector('[data-next]');
 const previousBtn = document.querySelector('[data-previous]');
 const jumpBtn = document.querySelectorAll('.jump-btn');
 
+slideShow();
+
 nextBtn.addEventListener('click', () => {
   nextSlide(1);
 });
+
 previousBtn.addEventListener('click', () => {
   nextSlide(-1);
 })
-jumpBtn.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    jumpToSlide(btn.dataset.values);
-  })
-})
+
+// Indicates active slide with red border.
+indicateActive(jumpBtn, 'active');
+
+// Jumps between slides.
+jumpToSlide(jumpBtn);
 
 addClassOnHover(menu, hidden, 'hidden');
 addClassOnHover(hidden, hidden, 'hidden');
